487fbcb075521d85e24cbb2a2490b07003c03c67
Kevylujan <kevylujanbravo@gmail.com>
2024-11-06 21:58:53 -0500

    Actualizacion mapa de calor

--- secciones/mapa_calor.py
+++ secciones/mapa_calor.py
@@ -12,9 +12,17 @@ from streamlit_folium import st_folium
 def generar_mapa_calor(df3, centro_df):
     st.subheader("Mapa de Calor de Centros de Vacunación")
     
-    # Mapa de Calor
-    mapa = folium.Map(location=[centro_df['latitud'].values[0], centro_df['longitud'].values[0]], zoom_start=15)
-    heat_data = [[row['latitud'], row['longitud']] for index, row in df3.iterrows()]
-    HeatMap(heat_data).add_to(mapa)
-    st_folium(mapa, width=700, height=500)
+# Mapa de Calor con ajuste de tamaño
+mapa = folium.Map(location=[centro_df['latitud'].values[0], centro_df['longitud'].values[0]], zoom_start=15)
+heat_data = [[row['latitud'], row['longitud']] for index, row in df3.iterrows()]
+HeatMap(heat_data).add_to(mapa)
+st_folium(mapa, width=700, height=400)  # Ajuste de tamaño para reducir el espacio
+
+# Agregar espacio pequeño entre el mapa y la tabla
+st.markdown("<br>", unsafe_allow_html=True)
+
+# Tabla dinámica con filtros
+st.subheader("Tabla de Centros de Vacunación")
+st.dataframe(tabla_filtrada)
+
 
